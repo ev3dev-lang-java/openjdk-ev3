aabc7f85abc626cc69c00fdfa425263b2e906bdc
Jakub Vaněk <vanek.jakub4@seznam.cz>
2018-03-25 22:15:26 +0200

    Update patches, packages & paths for JDK10

--- build/fetch.sh
+++ build/fetch.sh
@@ -9,11 +9,10 @@ hg clone "$JAVA_REPO" "$JDKDIR"
 
 # clone the rest of the tree
 cd "$JDKDIR"
-bash ./get_source.sh
 
 # apply the EV3-specific patches
 #  - build flags for arm926ej-s
-patch -p1            < "$SCRIPTDIR/ev3.patch"
+patch -p1 < "$SCRIPTDIR/ev3.patch"
 #  - use the system-provided floating point implementation
-patch -p1 -d hotspot < "$SCRIPTDIR/float.patch"
+patch -p1 < "$SCRIPTDIR/float.patch"
 
