14cb451d258d3df4fb37677eb074d700cc7fd86d
Jakub Vaněk <vanek.jakub4@seznam.cz>
2018-03-28 04:17:59 +0200

    [scripts] make fetch script pass for already downloaded source

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -4,17 +4,22 @@ set -e
 cd "$(dirname ${BASH_SOURCE[0]})"
 source config.sh
 
-# clone the root project
-hg clone "$JAVA_REPO" "$JDKDIR"
+if [ ! -d "$JDKDIR" ]; then
 
-# enter the jdk repo
-cd "$JDKDIR"
+  # clone the root project
+  hg clone "$JAVA_REPO" "$JDKDIR"
 
-# clone the rest of the tree, if needed
-if [ -f "./get_source.sh" ]; then
-  bash ./get_source.sh
+  # enter the jdk repo
+  cd "$JDKDIR"
+
+  # clone the rest of the tree, if needed
+  if [ -f "./get_source.sh" ]; then
+    bash ./get_source.sh
+  fi
+
+  # apply the EV3-specific patches
+  patch -p1 -i "$SCRIPTDIR/$PATCHVER.patch"
+
+else
+  echo "Directory for JDK repository exists, assuming everything has been done already." 2>&1
 fi
-
-# apply the EV3-specific patches
-patch -p1 -i "$SCRIPTDIR/$PATCHVER.patch"
-
