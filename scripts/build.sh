4ea1b1ee4cb52571fe225354caef79f45b188cd5
Jakub Vaněk <vanek.jakub4@seznam.cz>
2018-12-12 20:05:27 +0100

    [script] Update build option documentation

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,18 +22,27 @@ elif [ "$AUTOGEN_STYLE" == "v2" ]; then
 fi
 
 ## Description ##
-# Use the downloaded JDK:      --with-boot-jdk=/opt/jdkcross/jdk-10
-# Cross-compiling for ARM:     --openjdk-target=arm-linux-gnueabi
-# Tune for ARM926EJ-S softfp:  --with-abi-profile=arm-ev3
+# Use the downloaded JDK:      --with-boot-jdk=<dir>
+# Cross-compiling:             --openjdk-target=<triplet>
+# Pick the correct ABI:        --with-abi-profile=<profile>
+# Build only the right VM:     --with-jvm-variants=<VM>
 # Disable GUI:                 --enable-headless-only
-# Help to find freetype:       --with-freetype-lib=/usr/lib/arm-linux-gnueabi
-#                              --with-freetype-include=/usr/include
-# Build only the right VM:     --with-jvm-variants=client
 # Add extra build flags:       --with-extra-cflags="-w -Wno-error -D__SOFTFP__"
 #                              --with-extra-cxxflags="-w -Wno-error -D__SOFTFP__"
 #   - Fix the build on new GCC: -w -Wno-error
 #   - Force softfloat runtime:  -D__SOFTFP__
-# Fix the "internal" string:   --with-version-string="10+46-ev3"
+# Fix the "internal" string:   --with-version-string="<version>"
+# Use correct JNI path:        --with-jni-libpath=<dir1>:<dir2>...
+# Use softfloat libraries:     --with-softfloat-lib=<softfloat.a> --with-softfloat-include=<dirname softfloat.h>
+# Use AdoptOpenJDK CA certs:   --with-cacerts-file=<path>
+# Use correct debug level      --with-debug-level=<level>
+# Help to find freetype:       --with-freetype-lib=/usr/lib/arm-linux-gnueabi
+#                              --with-freetype-include=/usr/include
+# Use system libraries:        --with-libjpeg=system
+#                              --with-giflib=system
+#                              --with-libpng=system
+#                              --with-zlib=system
+#                              --with-lcms=system
 # Fix for GCC and LTO objects: AR="arm-linux-gnueabi-gcc-ar"
 #                              NM="arm-linux-gnueabi-gcc-nm"
 #                              BUILD_AR="gcc-ar"
