62d11bf8b1dec05df97901ff9e4d0ab279c5f711
Jakub Vaněk <vanek.jakub4@seznam.cz>
2019-06-02 10:00:51 +0200

    [docker] Fix docker build issue, try 4

--- native/scripts/autorun.sh
+++ native/scripts/autorun.sh
@@ -1,1 +1,15 @@
-../../scripts/autorun.sh
+#!/bin/bash
+
+if [ "$AUTOBUILD" -eq "1" ] || [ "$AUTOBUILD" == "yes" ]; then
+    set -e
+
+    cd "$(dirname ${BASH_SOURCE[0]})"
+    source config.sh
+
+    ./prepare.sh
+    ./fetch.sh
+    ./build.sh
+    ./zip.sh
+else
+    exec bash --login
+fi
--- native/scripts/prepare.sh
+++ native/scripts/prepare.sh
@@ -1,1 +1,31 @@
-../../scripts/prepare.sh
+#!/bin/bash
+set -e
+
+cd "$(dirname ${BASH_SOURCE[0]})"
+source config.sh
+
+if [ ! -d "$HOSTJDK" ]; then
+  if [ ! -e "$HOSTJDK_FILE" ]; then
+    echo "[PREPARE] Downloading host JDK"
+    wget -nv "$HOSTJDK_URL" -O "$HOSTJDK_FILE"
+  else
+    echo "[PREPARE] Using cached host JDK archive"
+  fi
+  echo "[PREPARE] Unpacking host JDK"
+  tar -xf "$HOSTJDK_FILE" -C "$(dirname "$HOSTJDK")"
+else
+  echo "[PREPARE] Using cached host JDK directory"
+fi
+
+if [ ! -d "$JTREG" ]; then
+  if [ ! -e "$JTREG_FILE" ]; then
+    echo "[PREPARE] Downloading jtreg"
+    wget -nv "$JTREG_URL" -O "$JTREG_FILE"
+  else
+    echo "[PREPARE] Using cached jtreg archive"
+  fi
+  echo "[PREPARE] Unpacking jtreg"
+  tar -xf "$JTREG_FILE" -C "$(dirname "$JTREG")"
+else
+  echo "[PREPARE] Using cached jtreg directory"
+fi
